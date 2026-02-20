f630494dfa8608d6a90dec1d493a1780177e2f11
sakshiwadye8 <sakshiwadye@gmail.com>
2026-02-21 01:03:46 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,18 +4,27 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-define('SITEURL', 'https://your-render-url.onrender.com/');
+define('SITEURL', 'https://your-app-name.onrender.com/');
 
-$conn = mysqli_connect(
+$mysqli = mysqli_init();
+
+mysqli_ssl_set($mysqli, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $mysqli,
     'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
     'SjQXduyi66DiXMN.root',
     'jUO6KPFaGvZbpeDQ',
     'cricket_store',
-    4000
+    4000,
+    NULL,
+    MYSQLI_CLIENT_SSL
 );
 
-if(!$conn){
-    die("Database Connection Failed: " . mysqli_connect_error());
+if (!$mysqli) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+$conn = $mysqli;
 
 ?>
